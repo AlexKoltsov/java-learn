6cc73cbc49adf43d6116ec3261dc55d9c8ae53f7
Alexander Koltsov <alexander.koltsov.work@gmail.com>
2019-06-07 16:17:31 +0300

    basic module added

---
+++ basic/src/main/java/com/example/java_learn/basic/LoopFor.java
@@ -0,0 +1,19 @@
+package com.example.java_learn.basic;
+
+public class LoopFor {
+
+    public static void main(String[] args) {
+        int anchor = 8;
+        int sumOfEvenNumbers = 0;
+
+        for (int i = 0; i <= anchor; i++) {
+            if (i % 2 == 0)
+                sumOfEvenNumbers += i;
+            else
+                System.out.print(i + " ");
+        }
+        System.out.println();
+
+        System.out.println(sumOfEvenNumbers);
+    }
+}
